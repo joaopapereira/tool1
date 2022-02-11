67ba0a91ae441c2c9f89b580344bf80c6342774b
João Pereira <joaod@vmware.com>
2022-02-11 11:09:25 -0600

    do the thing
    Signed-off-by: João Pereira <joaod@vmware.com>

--- registry/build.ps1
+++ registry/build.ps1
@@ -1,4 +1,4 @@
 Write-Host Building registry binary and image
 $version=$(select-string -Path Dockerfile -Pattern "ENV DISTRIBUTION_VERSION").ToString().split()[-1].SubString(1)
-docker build -t registry .
-docker tag registry:latest registry:$version
+docker build -t ghcr.io/joaopapereira/tool1/registry .
+docker tag ghcr.io/joaopapereira/tool1/registry:latest registry:$version
